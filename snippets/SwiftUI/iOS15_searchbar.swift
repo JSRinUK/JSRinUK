3c8cf116121756b9ff5066d85b6ab28b90156aa6
J Rudderham <99021236+JSRinUK@users.noreply.github.com>
2022-03-10 13:57:25 +0000

    add code for iOS15 SearchBar
    You'll need to change the contents of the NavigationLink(s), ObservedObject, and model.property to suit your own list of values.

--- snippets/SwiftUI/iOS15_searchbar.swift
+++ snippets/SwiftUI/iOS15_searchbar.swift
@@ -1,1 +1,39 @@
-// SearchBar
+//
+//  ContentView.swift
+//  SeacrBar
+//
+//  Created by Jonathan Rudderham on 10/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  @ObservedObject var model: DataUtils // change to own model
+  @State private var searchText = ""
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(searchResults) { data in 
+                                // change to suit
+                                NavigationLink(data.title,
+                                       destination: VideoScreen(video: data))
+                        
+        }
+      }
+      .navigationTitle(Text("Results:"))
+    }
+    .searchable(text: $searchText)
+  }
+  
+  var searchResults: [DataModel] {
+    if searchText.isEmpty {
+      return model.jsonDataFile // change to own property
+    } else {
+      return model.jsonDataFile.filter { $0.title.contains(searchText)}
+    }
+  }
+  
+}
+
